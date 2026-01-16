8806235a69e8e8ca566deb53342ae155e29255bb
Vishal Wankhade <vwankhade120@.com>
2026-01-17 03:06:10 +0530

    first Dev commit: routing added , basic file structure and routing and base of navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Navbar from "./components/common/Navbar";
+import AppRoutes from "./routes/AppRoutes";
+import Footer from "./components/common/Footer";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-    <div className="App">
-      Let's build a Debonair E-commerce Application!
-    </div>
+      <Navbar />
+      <AppRoutes />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
