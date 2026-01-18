a695783778d46e95f1a34a59c13843396d4cf390
Vishal Wankhade <vwankhade120@.com>
2026-01-18 16:21:01 +0530

    fix: resolve merge conflict in tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-<<<<<<< Updated upstream
-    extend: {},
-=======
     extend: {
-      fontFamily: {
+     fontFamily: {
         sans: ["Inter", "ui-sans-serif", "system-ui"],
         brand: ["Platypi", "serif"],
         subheading: ['Inter', 'sans-serif'],
       },
     },
->>>>>>> Stashed changes
   },
   plugins: [],
 };
