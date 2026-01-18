bdde7097d55cde60727582ee024350b74a61cdb0
Vishal Wankhade <vwankhade120@.com>
2026-01-18 15:54:11 +0530

    added hero section

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,3 @@
-<<<<<<< Updated upstream
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-=======
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
@@ -11,7 +5,6 @@ import { Provider } from "react-redux";
 import { store } from "./redux/Store";
 import App from "./App";
 import "./index.css";
->>>>>>> Stashed changes
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
